58806126cacdcbbc6503a293d2e2ab4ae80c9f1a
Dello <jakedell95@gmail.com>
2017-10-30 16:54:32 +1100

    Comments

--- src/FileWriter.cpp
+++ src/FileWriter.cpp
@@ -11,20 +11,24 @@ using namespace std;
 
 string FileWriter::cwdVar = "/home/dello/EFLS/build/results/temp/";
 
+// Initilise timeing profiler
 FileWriter::FileWriter(const char* timeName) {
     this->timeName = timeName;
     start();
 }
 
+// Start time of timing profiler
 void FileWriter::start() {
     startTime = chrono::high_resolution_clock::now();
 }
 
+// Calculates the time span of the requested timing profiler
 void FileWriter::end() {
     chrono::duration<double> time_span = chrono::duration_cast<chrono::duration<double>>(chrono::high_resolution_clock::now() - startTime);
     write(timeName, time_span.count());
 }
 
+// The write method of the timing profiler
 void FileWriter::write(const char* input, double timeDifference){
     ofstream file;
     file.open(cwd("data.txt").c_str(), ios::app);
@@ -34,6 +38,7 @@ void FileWriter::write(const char* input, double timeDifference){
     file.close();
 }
 
+// Writes a log message to file
 void FileWriter::write(const char* input){
     ofstream file;
     file.open(cwd("data.txt").c_str(), ios::app);
@@ -42,6 +47,7 @@ void FileWriter::write(const char* input){
     file.close();
 }
 
+// Returns the current working dictionary to save results to
 string FileWriter::cwd(const char* input) {
     stringstream message;
     message << "/home/dello/EFLS/build/results/" << input;
@@ -49,6 +55,7 @@ string FileWriter::cwd(const char* input) {
     return messageStr;
 }
 
+// Returns the current working dictionary of the results from a specific interation of the Decision Engine
 string FileWriter::cwdVariable(const char* input) {
     stringstream message;
     message << cwdVar << input;
@@ -56,6 +63,7 @@ string FileWriter::cwdVariable(const char* input) {
     return messageStr;
 }
 
+// Changes the variable working dictionary
 void FileWriter::setCwdVariable(const char* input) {
     cwdVar = input;
     mkdir(cwdVar.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
